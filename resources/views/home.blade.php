9e59cf6b3b0c0bf994a38e3de84705eaca2adc74
livi83 <livia.kelebercova@gmail.com>
2020-06-16 17:44:42 +0200

    homeblade php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,3 @@
-
-Routes:
-// úplne na konci 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/home', 'HomeController@index');
-    Route::resource('tests', 'TestsController');
-    Route::resource('roles', 'RolesController');
-    Route::post('roles_mass_destroy', ['uses' => 'RolesController@massDestroy', 'as' => 'roles.mass_destroy']);
-    Route::resource('users', 'UsersController');
-    Route::post('users_mass_destroy', ['uses' => 'UsersController@massDestroy', 'as' => 'users.mass_destroy']);
-    Route::resource('user_actions', 'UserActionsController');
-    Route::resource('topics', 'TopicsController');
-    Route::post('topics_mass_destroy', ['uses' => 'TopicsController@massDestroy', 'as' => 'topics.mass_destroy']);
-    Route::resource('questions', 'QuestionsController');
-    Route::post('questions_mass_destroy', ['uses' => 'QuestionsController@massDestroy', 'as' => 'questions.mass_destroy']);
-    Route::resource('questions_options', 'QuestionsOptionsController');
-    Route::post('questions_options_mass_destroy', ['uses' => 'QuestionsOptionsController@massDestroy', 'as' => 'questions_options.mass_destroy']);
-    Route::resource('results', 'ResultsController');
-    Route::post('results_mass_destroy', ['uses' => 'ResultsController@massDestroy', 'as' => 'results.mass_destroy']);
-});
- 
-Homeblade.php
 @extends('layouts.app')
 
 @section('content')
@@ -49,13 +27,7 @@ Homeblade.php
                     </div>
                 </div>
             </div>
-            <a href="#" class="btn btn-success">Urobiť kvíz</a>
+            <a href="{{ route('tests.index') }}" class="btn btn-success">Urobiť kvíz</a>
         </div>
     </div>
 @endsection
-
- 
-
-
- 
-
